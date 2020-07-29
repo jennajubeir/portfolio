4fa3c1c273d6270aa58c810ca0f65811adbeb51c
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-07-29 07:46:32 -0400

    fixed index

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fonts/OpenSans-Regular.ttf'
-import './fonts/OpenSans-Bold.ttf'
-import { StateProvider } from './data/context';
 
-ReactDOM.render(<StateProvider><App /></StateProvider>, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
