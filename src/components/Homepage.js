a804dc8cd8adfc94be7859c1166dd7b0d06a9aea
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-08-03 16:37:41 -0500

    homepage fix

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -24,32 +24,23 @@ const Homepage = () => {
     <div>
       <h1 className="welcome">Hi, my name is Jenna Jubeir!</h1>
       <p className="introduction">
-        I study computer science & chemistry student at Northwestern University
-        and am interested in product management and product design.
+        I study computer science & chemistry at Northwestern University and am
+        interested in product management and product design.
       </p>
       <div class="ui center aligned container" id="outer">
-        <Grid centered id="container">
-          <Grid.Column className="column" mobile={16} tablet={8} computer={4}>
-            <div className="card">
-              <h1 className="desc">check out my projects</h1>
-            </div>
-          </Grid.Column>
-          <Grid.Column className="column" mobile={16} tablet={8} computer={4}>
-            <div className="card">
-              <h1 className="desc">take a look at my resume</h1>
-            </div>
-          </Grid.Column>
-          <Grid.Column className="column" mobile={16} tablet={8} computer={4}>
-            <div className="card">
-              <h1 className="desc">learn more about me</h1>
-            </div>
-          </Grid.Column>
-          <Grid.Column className="column" mobile={16} tablet={8} computer={4}>
-            <div className="card">
-              <h1 className="desc">contact me</h1>
-            </div>
-          </Grid.Column>
-        </Grid>
+        <Button className="card">
+          <h1 className="desc">check out my projects</h1>
+        </Button>
+        <Button className="card">
+          <h1 className="desc">take a look at my resume</h1>
+        </Button>
+        <Button className="card">
+          <h1 className="desc">learn more about me</h1>
+        </Button>
+
+        <Button className="card">
+          <h1 className="desc">contact me</h1>
+        </Button>
       </div>
     </div>
   );
