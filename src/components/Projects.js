d8f9a8db5d6095613ccb6dc99fd853d54f77b884
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-08-04 22:36:42 -0500

    project outline

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,106 +12,80 @@ import {
   Icon,
   Card,
   Image,
+  Label,
 } from "semantic-ui-react";
 import "../styles/Projects.css";
-import ex from "../ex.jpg";
+import nodi from "../images/NODI.png";
+import tunepad from "../images/TUNEPAD.png";
+import peerpal from "../images/PEERPAL.png";
+import elcare from "../images/ELCARE.png";
+import itemshare from "../images/itemshare.png";
+import fyf from "../images/fyf.png";
+import sw from "../images/safetywatch.png";
 const Projects = () => (
   <div>
     <h1 className="title">my projects</h1>
-    <div class="ui center aligned container" id="outer">
-      <Grid centered id="container">
-        <Grid.Column className="column" mobile={16} tablet={8} computer={5}>
-          <Card className="card">
-            <Image src={ex} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>Project name</Card.Header>
-              <Card.Meta>date</Card.Meta>
-              <Card.Description> description </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>boxes with skills</a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-        <Grid.Column className="column" mobile={16} tablet={8} computer={5}>
-          <Card className="card">
-            <Image src={ex} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>Project name</Card.Header>
-              <Card.Meta>date</Card.Meta>
-              <Card.Description>brief description </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>boxes with skills</a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-        <Grid.Column className="column" mobile={16} tablet={8} computer={5}>
-          <Card className="card">
-            <Image src={ex} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>Project name</Card.Header>
-              <Card.Meta>date</Card.Meta>
-              <Card.Description>brief description </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>boxes with skills</a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-        <Grid.Column className="column" mobile={16} tablet={8} computer={5}>
-          <Card className="card">
-            <Image src={ex} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>Project name</Card.Header>
-              <Card.Meta>date</Card.Meta>
-              <Card.Description>brief description </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>boxes with skills</a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-        <Grid.Column className="column" mobile={16} tablet={8} computer={5}>
-          <Card className="card">
-            <Image src={ex} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>Project name</Card.Header>
-              <Card.Meta>date</Card.Meta>
-              <Card.Description>brief description </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>boxes with skills</a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-        <Grid.Column className="column" mobile={16} tablet={8} computer={5}>
-          <Card className="card">
-            <Image src={ex} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>Project name</Card.Header>
-              <Card.Meta>date</Card.Meta>
-              <Card.Description>brief description </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>boxes with skills</a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-        <Grid.Column className="column" mobile={16} tablet={8} computer={5}>
-          <Card className="card">
-            <Image src={ex} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>Project name</Card.Header>
-              <Card.Meta>date</Card.Meta>
-              <Card.Description>brief description </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>boxes with skills</a>
-            </Card.Content>
-          </Card>
-        </Grid.Column>
-      </Grid>
+    <div class="ui center aligned container" id="pouter">
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/projects")}
+      >
+        <img class="ui centered medium image" src={peerpal}></img>
+        <h1 className="pdesc">Full Stack Developer</h1>
+      </Button>
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/projects")}
+      >
+        <img class="ui centered medium image" src={nodi}></img>
+        <h1 className="pdesc">Co-Founder & Front-End Team Lead</h1>
+      </Button>
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/aboutme")}
+      >
+        <img class="ui centered small image" src={tunepad}></img>
+        <h1 className="pdesc">Research Assistant</h1>
+      </Button>
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/aboutme")}
+      >
+        <img class="ui centered small image" src={elcare}></img>
+        <h1 className="pdesc">Team Lead</h1>
+      </Button>
+    </div>
+    <div class="ui center aligned container" id="pouter">
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/aboutme")}
+      >
+        <img class="ui centered medium image" src={fyf}></img>
+        <h1 className="pdesc">
+          Full Stack Developer & Client Communication Lead
+        </h1>
+      </Button>
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/aboutme")}
+      >
+        <img class="ui centered small image" src={itemshare}></img>
+        <h1 className="pdesc">Full Stack Developer</h1>
+      </Button>
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/aboutme")}
+      >
+        <img class="ui centered small image" src={sw}></img>
+        <h1 className="pdesc">Full Stack Developer</h1>
+      </Button>
+      <Button
+        className="pcard"
+        onClick={() => (window.location.href = "/aboutme")}
+      >
+        <img class="ui centered small image" src={sw}></img>
+        <h1 className="pdesc">DTC PROJECT</h1>
+      </Button>
     </div>
   </div>
 );
