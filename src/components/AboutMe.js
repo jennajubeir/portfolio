b96194a69f660c50c5cda6b4293888d773bd8b4c
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-08-12 08:25:36 -0400

    work on the plane

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,10 +9,34 @@ import {
   Segment,
   Row,
   Button,
+  Image,
   Icon,
 } from "semantic-ui-react";
+import jenna from "../../src/jenna.jpg";
+import "../styles/AboutMe.css";
 
 const AboutMe = () => {
-  return <div>about me!</div>;
+  return (
+    <Container>
+      <h1 className="welcome"> let's get to know each other </h1>
+      <div class="ui center aligned container" id="aouter">
+        <div className="am">
+          <p>
+            It is so nice to meet you! My name is Jenna. I am from Washington,
+            DC, and I think the most defining characteristic of that is my
+            absolute love for the air and space museum.
+          </p>
+          <p>
+            In my free time I love hiking and eating ice cream, with most
+            sundays filled with both. Ice cream takes up most of my sweet tooth,
+            but I also like to bake, and overfeed my mother with all sorts of
+            cakes and cupcakes.
+          </p>
+          <p></p>
+        </div>
+        <img id="me" class="ui centered medium image" src={jenna}></img>
+      </div>
+    </Container>
+  );
 };
 export default AboutMe;
